23ddbb3be18a0f08c689583c2e4c3c9d74c8541a
Peter Close <pclose99@gmail.com>
2022-04-25 21:12:10 -0400

    add middleware 2

--- index.js
+++ index.js
@@ -53,3 +53,63 @@ app.use(express.static('./public'));
 // Allow JSON body messages on all endpoints
 app.use(express.json())
 
+
+// Extend express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up port
+args["port"];
+
+// Add port
+var HTTP_PORT = args.port || 5000;
+
+
+
+if(args.log == 'false') {
+    console.log("Not creating a new access.log")
+}
+else {
+    // Use morgan for logging to files
+    const WRITESTREAM = fs.createWriteStream('./data/log/access.log', { flags: 'a' })
+    app.use(morgan('combined', { stream: WRITESTREAM }))
+}
+
+// Start an app server
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+});
+
+
+// Add Middleware
+app.use( (req, res, next) => {
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    }
+    console.log(logdata)
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
+    next();
+})
+
+
+if(args.debug === true) {
+    app.get('/app/log/access/', (req, res) => {
+        const stmt = db.prepare("SELECT * FROM accesslog").all()
+        res.status(200).json(stmt)
+    });
+    app.get('/app/error', (req, res) => {
+        throw new Error('Error test successful.')
+    });
+}   
+
+
